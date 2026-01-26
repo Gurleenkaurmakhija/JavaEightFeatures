bfceb8ea912ce6d91c217b306730871f0dc582ff
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-01-26 21:06:25 +0530

    [GurleenKaur]: added assignments for virtual classroom

--- src/main/java/com/gevernova/day1/virtualclassroom/Assignment.java
+++ src/main/java/com/gevernova/day1/virtualclassroom/Assignment.java
@@ -3,16 +3,16 @@ package com.gevernova.day1.virtualclassroom;
 import java.util.HashMap;
 
 public class Assignment {
-    String title;
-    HashMap<Student, Integer> grades = new HashMap<>();
 
-    // constructor
+    String title;
+    HashMap<Student, Integer> grades = new HashMap<>(); // stores students and their marks
+
     Assignment(String title) {
         this.title = title;
     }
 
     void submitAssignment(Student s) {
-        grades.put(s, null);
+        grades.put(s, null); // marks not assigned yet
     }
 
     void gradeAssignment(Student s, int marks) {
