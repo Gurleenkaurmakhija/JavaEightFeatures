a154f815d5f1157beafa55126c8a8d60d13d3d1c
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-01-26 23:40:36 +0530

    [GurleenKaur]:Converting strings to uppercase

--- src/main/java/com/gevernova/day3/methodrefernce/touppercase/Main.java
+++ src/main/java/com/gevernova/day3/methodrefernce/touppercase/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         List<String> words = Arrays.asList("apple", "banana", "cherry");
-
         words.stream()
                 .map(String::toUpperCase)   // method reference
                 .forEach(System.out::println); // method reference
