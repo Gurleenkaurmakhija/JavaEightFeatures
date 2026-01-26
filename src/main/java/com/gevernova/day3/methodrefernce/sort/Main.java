f759461925e4ded84f9af081e6125bea9a699339
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-01-26 23:38:07 +0530

    [GurleenKaur]:Sorting the strings

--- src/main/java/com/gevernova/day3/methodrefernce/sort/Main.java
+++ src/main/java/com/gevernova/day3/methodrefernce/sort/Main.java
@@ -5,13 +5,16 @@ import java.util.Collections;
 import java.util.List;
 //uc3
 public class Main {
+
     public static void main(String[] args) {
 
         List<String> words = Arrays.asList("Banana", "apple", "Orange");
 
+        // sort strings ignoring case
         Collections.sort(words, String::compareToIgnoreCase);
 
         System.out.println(words);
     }
 }
+
 
