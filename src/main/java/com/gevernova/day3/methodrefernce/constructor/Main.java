99f7a7e36f4b856a73242d16c775f2ec01550292
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-01-26 23:29:50 +0530

    [GurleenKaur]:Using the shape creator

--- src/main/java/com/gevernova/day3/methodrefernce/constructor/Main.java
+++ src/main/java/com/gevernova/day3/methodrefernce/constructor/Main.java
@@ -2,6 +2,7 @@ package com.gevernova.day3.methodrefernce.constructor;
 
 public class Main {
     public static void main(String[] args) {
+
         //constructor refernce
         ShapeCreator creator = Rectangle::new;
         Rectangle r = creator.create(5, 6);
