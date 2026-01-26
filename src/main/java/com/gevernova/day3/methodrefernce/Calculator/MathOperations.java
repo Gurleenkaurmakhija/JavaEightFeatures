11b845fa02935e4ce5ea509d8e4476b7a586374b
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-01-26 23:15:41 +0530

    [GurleenKaur]:Defining the functionalities of calculator

---
+++ src/main/java/com/gevernova/day3/methodrefernce/Calculator/MathOperations.java
@@ -0,0 +1,20 @@
+package com.gevernova.day3.methodrefernce.Calculator;
+
+class MathOperations {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    static int divide(int a, int b) {
+        return a / b;
+    }
+}
