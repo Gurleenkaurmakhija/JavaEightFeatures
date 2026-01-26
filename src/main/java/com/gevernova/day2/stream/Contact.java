2aeda941a2a04bf8b9bf538c0671393864af8b0f
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-01-26 23:06:20 +0530

    [GurleenKaur]:Created format for contacts

--- src/main/java/com/gevernova/day2/stream/Contact.java
+++ src/main/java/com/gevernova/day2/stream/Contact.java
@@ -1,11 +1,16 @@
 package com.gevernova.day2.stream;
 
 public class Contact {
+
+    // contact details
     private String firstName, lastName, address, city, state, email;
     private int zip;
     private long phoneNumber;
 
-    public Contact(String firstName, String lastName, String address, String city, String state, String email,int zip, long phoneNumber) {
+    // constructor to initialize contact
+    public Contact(String firstName, String lastName, String address,
+                   String city, String state, String email,
+                   int zip, long phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -15,70 +20,28 @@ public class Contact {
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public String getLastName() {
-        return lastName;
-    }
+    // getters
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getAddress() { return address; }
+    public String getCity() { return city; }
+    public String getState() { return state; }
+    public String getEmail() { return email; }
+    public int getZip() { return zip; }
+    public long getPhoneNumber() { return phoneNumber; }
 
-    public String getAddress() {
-        return address;
-    }
+    // setters
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setAddress(String address) { this.address = address; }
+    public void setCity(String city) { this.city = city; }
+    public void setState(String state) { this.state = state; }
+    public void setEmail(String email) { this.email = email; }
+    public void setZip(int zip) { this.zip = zip; }
+    public void setPhoneNumber(long phoneNumber) { this.phoneNumber = phoneNumber; }
 
-    public String getCity() {
-        return city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getZip() {
-        return zip;
-    }
-
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-
-    public void setPhoneNumber(long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
+//     contact information
     @Override
     public String toString() {
         return firstName + " " + lastName +
