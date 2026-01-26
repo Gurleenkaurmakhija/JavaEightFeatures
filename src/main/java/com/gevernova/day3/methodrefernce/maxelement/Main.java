bb18e9da9176bf0c60f1b454651d42910857eef5
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-01-26 23:34:12 +0530

    [GurleenKaur]:Finding the maximum element

--- src/main/java/com/gevernova/day3/methodrefernce/maxelement/Main.java
+++ src/main/java/com/gevernova/day3/methodrefernce/maxelement/Main.java
@@ -8,6 +8,7 @@ import java.util.List;
 //uc6
 public class Main {
 
+    // finds maximum element using comparator
     static <T> T findMax(List<T> list, Comparator<T> comp) {
         return Collections.max(list, comp);
     }
@@ -16,7 +17,9 @@ public class Main {
 
         List<Integer> nums = Arrays.asList(10, 50, 30);
 
+        // method reference for comparison
         Integer max = findMax(nums, Integer::compareTo);
         System.out.println(max);
     }
 }
+
