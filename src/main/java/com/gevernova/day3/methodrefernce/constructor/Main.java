a7002057148ce7838bc67c32fd0f22a7ccd06167
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-01-26 23:28:26 +0530

    using the shapecreator

--- src/main/java/com/gevernova/day3/methodrefernce/constructor/Main.java
+++ src/main/java/com/gevernova/day3/methodrefernce/constructor/Main.java
@@ -1,24 +1,5 @@
 package com.gevernova.day3.methodrefernce.constructor;
 
-//uc4
-interface ShapeCreator {
-    Rectangle create(int l, int b);
-    //rectangle obj as return type
-}
-
-class Rectangle {
-    int l, b;
-
-    Rectangle(int l, int b) {
-        this.l = l;
-        this.b = b;
-    }
-
-    void area() {
-        System.out.println("Area = " + (l * b));
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         //constructor refernce
